2d3c2e7ab5c93a836d2c9160473d0b924d38a691
Brian Bycynski <high5bri@hotmail.com>
2022-03-09 04:10:13 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,56 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      date: new Date(2022, 6, 25),
+      title: 'Car Insurance 1',
+      price: 294.67,
+    },
+    {
+      id: 2,
+      date: new Date(2023, 6, 25),
+      title: 'Car Insurance 2',
+      price: 295.67,
+    },
+    {
+      id: 3,
+      date: new Date(2024, 6, 25),
+      title: 'Car Insurance 3',
+      price: 296.67,
+    },
+    {
+      id: 4,
+      date: new Date(2024, 6, 25),
+      title: 'Car Insurance 4',
+      price: 297.67,
+    },
+  ];
+
   return (
     <div className='App'>
       <h1>Let's Go</h1>
-      <ExpenseItem />
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        price={expenses[0].price}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        price={expenses[1].price}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        price={expenses[2].price}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        price={expenses[3].price}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,17 +5,13 @@ import React from 'react';
 
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 6, 25);
-  const expenseTitle = 'Car Insurance';
-  const expensePrice = 294.67;
-
+function ExpenseItem({ date, title, price }) {
   return (
     <div className='expense-item'>
-      <div className=''>{expenseDate.toISOString()}</div>
+      <div className=''>{date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expensePrice}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${price}</div>
       </div>
     </div>
   );
